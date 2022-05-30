3ee1c9ea4a9dea32a15aba35bbf7657ee5ac1849
Kajutin <kajuskatinas@gmail.com>
2022-05-30 20:36:33 +0300

    Got to a "Hello, World!" stage. Can be accessed on http://localhost:8080/greeting once you have the Spring web service running.

---
+++ src/main/java/lt/viko/eif/kkvmavva/restfulwebservice/Greeting.java
@@ -0,0 +1,20 @@
+package lt.viko.eif.kkvmavva.restfulwebservice;
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
