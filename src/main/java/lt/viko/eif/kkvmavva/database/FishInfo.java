9e10de5773377fafea392f163bafcc1ad72008f4
Valerija-Mur <s037936@ad.viko.lt>
2022-06-01 18:36:19 +0300

    Added variables to FishInfo Class

--- src/main/java/lt/viko/eif/kkvmavva/database/FishInfo.java
+++ src/main/java/lt/viko/eif/kkvmavva/database/FishInfo.java
@@ -1,5 +1,17 @@
 package lt.viko.eif.kkvmavva.database;
 
+/**
+ *
+ */
 public class FishInfo {
+    private String Name;
+    private String WaterType;
+    private String Location;
+    private String SecurityStatus;
+    private String BestToCatchSeason;
+    private String FishBreedingSeason;
+    private String IllegalToCatchSeason;
+    private Integer MinimumLegalSizeToCatch;
+    private String Description;
 
 }
